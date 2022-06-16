0599c6db521e9829bf647e49e350bc08f7bf21ad
chenweikai <chenwk891@gmail.com>
2022-06-16 00:02:50 -0700

    update

--- data_generation/src/batch_generate.cpp
+++ data_generation/src/batch_generate.cpp
@@ -23,12 +23,6 @@ int main(int argc, char** argv) {
   std::string out_recon_ply_dir = "../output/PLY";
   std::string todo_filename = "../data/todo.txt";  // a txt file containing the names of the subjects to be computed
   std::vector<std::string> todo_list;
-  std::ifstream fin;
-  fin.open(todo_filename);
-  std::string name;
-  while (fin >> name) {
-    todo_list.push_back(name);
-  }
 
   int octree_depth = 7;
   int write_ply = 0;
@@ -36,7 +30,8 @@ int main(int argc, char** argv) {
   int write_obj = 1;
 
   if (argc < 9) {
-    std::cout << "usage: ./batch_generate input_mesh_dir out_sdf_dir out_recon_obj_dir out_ply_dir octree_depth flag_writeSDF [Default: 1] flag_writeOBJ [Default: 1] flag_writePLY [Default: 0] [todo_list.txt (optional)]" << std::endl;
+    std::cout << "usage: ./batch_generate input_mesh_dir out_sdf_dir out_recon_obj_dir out_ply_dir \
+                  octree_depth flag_writeSDF [Default: 1] flag_writeOBJ [Default: 1] flag_writePLY [Default: 0] [todo_list.txt (optional)]" << std::endl;
     std::cout << "Insufficient input argument. Please refer to the usage info for more details. Currently run the default setting for illustration!" << std::endl;
   } else {
     input_data_dir = std::string(argv[1]);
@@ -52,10 +47,11 @@ int main(int argc, char** argv) {
       todo_filename = std::string(argv[9]);
     } else {
       // If no todo list is provided, collect all the samples under the input data directory.
+      std::cout << "Adding all .obj files under the input data directory to the todo list ..." << std::endl;
       todo_list.clear();
       for (const auto & entry : fs::directory_iterator(input_data_dir)) {
         std::string path = entry.path();
-        std::string ext = path.substr(path.find_last_of("."));
+        std::string ext = path.substr(path.find_last_of(".") + 1);
         // Currently only load obj files
         if (ext == "obj") {
           std::string base_filename = path.substr(path.find_last_of("/\\") + 1, path.find_last_of(".") - path.find_last_of("/\\") - 1);
