102a9b2f4f1b78182de06cec28834756f85f0336
Weikai <chenwk891@gmail.com>
2022-06-15 17:02:26 -0700

    minor change on IO

--- data_generation/src/batch_generate.cpp
+++ data_generation/src/batch_generate.cpp
@@ -23,8 +23,14 @@ int main(int argc, char** argv) {
   std::string out_recon_ply_dir = "../output/PLY";
   std::string todo_filename = "../data/todo.txt";  // a txt file containing the names of the subjects to be computed
   std::vector<std::string> todo_list;
+  std::ifstream fin;
+  fin.open(todo_filename);
+  std::string name;
+  while (fin >> name) {
+    todo_list.push_back(name);
+  }
 
-  int octree_depth = 9;
+  int octree_depth = 7;
   int write_ply = 0;
   int write_sdf = 1;
   int write_obj = 1;
@@ -33,7 +39,6 @@ int main(int argc, char** argv) {
     std::cout << "usage: ./batch_generate input_mesh_dir out_sdf_dir out_recon_obj_dir out_ply_dir octree_depth flag_writeSDF [Default: 1] flag_writeOBJ [Default: 1] flag_writePLY [Default: 0] [todo_list.txt (optional)]" << std::endl;
     std::cout << "Insufficient input argument. Please refer to the usage info for more details. Currently run the default setting for illustration!" << std::endl;
   } else {
-
     input_data_dir = std::string(argv[1]);
     out_sdf_dir = std::string(argv[2]);
     out_recon_obj_dir = std::string(argv[3]);
@@ -43,15 +48,32 @@ int main(int argc, char** argv) {
     write_obj = atoi(argv[7]);
     write_ply = atoi(argv[8]);
 
+    if (argc >= 10) {
+      todo_filename = std::string(argv[9]);
+    } else {
+      // If no todo list is provided, collect all the samples under the input data directory.
+      todo_list.clear();
+      for (const auto & entry : fs::directory_iterator(input_data_dir)) {
+        std::string path = entry.path();
+        std::string ext = path.substr(path.find_last_of("."));
+        // Currently only load obj files
+        if (ext == "obj") {
+          std::string base_filename = path.substr(path.find_last_of("/\\") + 1, path.find_last_of(".") - path.find_last_of("/\\") - 1);
+          todo_list.push_back(base_filename);
+        }
+      }
+    }
+  }
 
+  if (todo_list.empty()) {
     std::ifstream fin;
     fin.open(todo_filename);
     std::string name;
     while (fin >> name) {
       todo_list.push_back(name);
     }
-    std::cout << std::endl;
   }
+
 
   if (!fs::exists(input_data_dir)) {
     std::cout << "Cannot find the input data directory!" << std::endl;
@@ -93,7 +115,7 @@ int main(int argc, char** argv) {
     std::string recon_obj_name =  out_recon_obj_dir + "/" + subject + ".obj";
     std::string output_ply_name = out_recon_obj_dir + "/" + subject + ".ply";
 
-    GenerateOctree3psdfSamples(input_obj_name, sdf_name, recon_obj_name, output_ply_name, octree_depth, write_ply, write_obj, write_sdf);
+    GenerateOctree3psdfSamples(input_obj_name, sdf_name, recon_obj_name, output_ply_name, octree_depth, write_sdf, write_obj, write_ply);
   }
 
   return 1;
